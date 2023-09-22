b7d696acf6f821690c364bba35075a7de9d82b89
Erkan Zafer Dolgun <erkan@Erkans-MacBook-Air.local>
2023-09-22 20:22:41 +0300

    CPP08

---
+++ ex01/Span.hpp
@@ -0,0 +1,32 @@
+#ifndef SPAN_HPP
+ #define SPAN_HPP
+
+#include <iostream>
+#include <list>
+#include <exception>
+
+class Span
+{
+private:
+    std::list<int>  _list;
+    unsigned int    _n;
+
+    Span( void );
+
+public:
+    Span( unsigned int );
+    Span( const Span& );
+    ~Span( void );
+
+    Span&               operator=( const Span& );
+    void                addNumber( int );
+    void                addNumber( std::list<int>::const_iterator, std::list<int>::const_iterator );
+    unsigned int        longestSpan( void ) const;
+    unsigned int        shortestSpan( void ) const;
+
+    const std::list< int >*   getList( void ) const;
+};
+
+std::ostream& operator<<( std::ostream&, const Span& );
+
+#endif
